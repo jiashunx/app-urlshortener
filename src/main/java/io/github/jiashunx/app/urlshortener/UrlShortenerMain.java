e8bf679a31cf6ddbcddd9cc64649ba8ccb0f15be
jiashunx <wenjiashun@163.com>
2021-03-11 10:11:31 +0800

    add pom.xml

---
+++ src/main/java/io/github/jiashunx/app/urlshortener/UrlShortenerMain.java
@@ -0,0 +1,16 @@
+package io.github.jiashunx.app.urlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author jiashunx
+ */
+@SpringBootApplication
+public class UrlShortenerMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerMain.class, args);
+    }
+
+}
